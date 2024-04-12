db687d8638787e8f614a3e956debcc5b5e2d96f3
anbke11 <166827423+anbke11@users.noreply.github.com>
2024-04-12 20:21:49 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+meme.dict = { 
+            "LOL" - "tanggapan terhadap sesuatu yang lucu",
+            "CRINGE" - "sesuatu yang aneh atau memalukan",
+            "ROFL" - "tanggapan terhadap lelucon",
+            "SHEESH" - "sedikit ketidaksetujuan",
+            "CREEPY" - "menakutkan, tidak menyenangkan",
+            "AGGRO" - "untuk menjadi agresif/marah"
+            }
+word = input("Ketik kata yang tidak kamu mengerti (gunakan huruf kapital semua!):")
+
+if word in meme_dict.keys():
+    print('makna kata:', meme_dict.[word])
+else:
+    print('Kata tidak ditemukan')
